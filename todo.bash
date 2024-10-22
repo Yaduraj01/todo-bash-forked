ee443d3b8b32158c632b6dc5bbfb55ef606ca785
Yaduraj01 <pawaryaduraj3232@gmail.com>
2024-10-22 20:54:48 +0530

    Invalid input handling

--- todo.bash
+++ todo.bash
@@ -3,76 +3,233 @@
 # More information below at the "Execution" section
 
 # Todo Function
-function todo(){
-	local conf="${HOME}/.config/.todo"
-	local iterVal mode array
-	[[ -f "${conf}" ]] ||
-	cat /dev/null > "${conf}"
-	sed -i '/^\s*$/d' "${conf}"
-	sed -i '/^$/d' "${conf}"
-	case "$1" in
-		-h|--help)	mode=0 ;;
-		-r|--remove)	mode=1 ;;
-		-a|--add)	mode=2 ;;
-		*[0-9]*)	mode=3 ;;
-		-q|--quiet)	mode=4 ;;
-		*)		mode=5 ;;
-	esac
-	if [[ "${mode}" =~ ^[0-5]$ ]]; then
-		if [[ "${mode}" -eq 0 ]]; then
-			cat << EOF
+function todo() {
+    local conf="${HOME}/.config/.todo"
+    [[ -f "${conf}" ]] || touch "${conf}"  # Create config file if it doesn't exist
+    sed -i '/^\s*$/d' "${conf}"  # Remove empty lines
 
-@Usage:	todo [INDEX]...
-     	todo [OPTIONS [INDEX|ITEM]...]...
+    local mode
+    case "$1" in
+        -h|--help)    mode=0 ;;
+        -r|--remove)  mode=1 ;;
+        -a|--add)     mode=2 ;;
+        *[0-9]*)      mode=3 ;;
+        -q|--quiet)   mode=4 ;;
+        *)            mode=5 ;;
+    esac
+
+    if [[ "${mode}" =~ ^[0-5]$ ]]; then
+        case "${mode}" in
+            0)  # Help message
+                cat << EOF
+
+@Usage: todo [INDEX]...
+      todo [OPTIONS [INDEX|ITEM]...]...
 List, add, or remove todo items.
 
 @OPTIONS:
-	-h,--help	This help message.
-	-r,--remove	Remove an item by INDEX number. 
-	-a,--add	Add an item by ITEM.
-	-q,--quiet	No error messages.
+    -h, --help      This help message.
+    -r, --remove    Remove an item by INDEX number. 
+    -a, --add       Add an item by ITEM.
+    -q, --quiet     No error messages.
 @INDEX:
-	Integers	Index number of item.
+    Integers        Index number of item.
 @ITEM:
-	String		Todo ITEM.
+    String          Todo ITEM.
 @EXAMPLES:
-	todo 		
-		List all items in todo list.
-	todo 1
-		List 1st ITEM in todo list. 
-	todo -a "Something to do"
-		Add a todo item.
-	todo -r 1
-		Remove item at index #1.
+    todo            List all items in todo list.
+    todo 1          List 1st ITEM in todo list. 
+    todo -a "Something to do"  Add a todo item.
+    todo -r 1       Remove item at index #1.
 
 EOF
-		elif [[ "${mode}" -eq 1 ]]; then
-			sed -i -e "${2}d" "${conf}"
-			return
-		elif [[ "${mode}" -eq 2 ]]; then
-			echo "$2" >> "${conf}"
-			return
-		elif [[ "${mode}" -eq 3 ]]; then
-			readarray -t array < "${conf}"
-			echo "${array[$((${1} - 1))]}"
-			return
-		else
-			readarray -t array < "${conf}"
-			if [[ ${#array[@]} -eq 0 ]]; then
-				[[ ${mode} -ne 4 ]] &&
-				echo "No items in the TODO list."
-				return
-			fi
-			for iterVal in "${!array[@]}"; do
-				echo "[$((${iterVal} + 1))]:${array[${iterVal}]}"
-			done
-			return
-		fi
-	fi
+                return 0
+                ;;
+            1)  # Remove an item
+                if [[ -z "$2" ]]; then
+                    echo "Error: Please provide an index number to remove."
+                    return 1
+                fi
+                if ! [[ "$2" =~ ^[0-9]+$ ]]; then
+                    echo "Error: Index must be a valid positive integer."
+                    return 1
+                fi
+                if ! sed -n "${2}p" "${conf}" >/dev/null; then
+                    echo "Error: No item found at index $2."
+                    return 1
+                fi
+                sed -i -e "${2}d" "${conf}" || {
+                    echo "Error: Failed to remove item."
+                    return 1
+                }
+                echo "Item $2 removed."
+                return 0
+                ;;
+            2)  # Add an item
+                if [[ -z "$2" ]]; then
+                    echo "Error: Please provide an item to add."
+                    return 1
+                fi
+                echo "$2" >> "${conf}" || {
+                    echo "Error: Failed to add item."
+                    return 1
+                }
+                echo "Item added: $2"
+                return 0
+                ;;
+            3)  # List a specific item
+                if ! [[ "$1" =~ ^[0-9]+$ ]]; then
+                    echo "Error: Index must be a valid positive integer."
+                    return 1
+                fi
+                readarray -t array < "${conf}"
+                if [[ -z "${array[$(( $1 - 1 ))]}" ]]; then
+                    echo "Error: No item found at index $1."
+                    return 1
+                fi
+                echo "${array[$(( $1 - 1 ))]}"
+                return 0
+                ;;
+            4)  # Quiet mode
+                return 0
+                ;;
+            5)  # List all items
+                readarray -t array < "${conf}"
+                if [[ ${#array[@]} -eq 0 ]]; then
+                    [[ "${mode}" -ne 4 ]] && echo "No items in the TODO list."
+                    return 0
+                fi
+                for index in "${!array[@]}"; do
+                    echo "[$(( index + 1 ))]: ${array[index]}"
+                done
+                return 0
+                ;;
+        esac
+    else
+        echo "Error: Invalid command. Use -h or --help for usage information."
+        return 1
+    fi
 }
-$(return >/dev/null 2>&1)
-if [ "$?" -eq "0" ]; then
-	complete -W '-h --help -r --remove -a --add -q --quiet' todo
+
+# Tab completion
+if [ "$_" == "$0" ]; then
+    complete -W '-h --help -r --remove -a --add -q --quiet' todo
+#!/usr/bin/env bash
+# This file can be sourced or executed.
+# More information below at the "Execution" section
+
+# Todo Function
+function todo() {
+    local conf="${HOME}/.config/.todo"
+    [[ -f "${conf}" ]] || touch "${conf}"  # Create config file if it doesn't exist
+    sed -i '/^\s*$/d' "${conf}"  # Remove empty lines
+
+    local mode
+    case "$1" in
+        -h|--help)    mode=0 ;;
+        -r|--remove)  mode=1 ;;
+        -a|--add)     mode=2 ;;
+        *[0-9]*)      mode=3 ;;
+        -q|--quiet)   mode=4 ;;
+        *)            mode=5 ;;
+    esac
+
+    if [[ "${mode}" =~ ^[0-5]$ ]]; then
+        case "${mode}" in
+            0)  # Help message
+                cat << EOF
+
+@Usage: todo [INDEX]...
+      todo [OPTIONS [INDEX|ITEM]...]...
+List, add, or remove todo items.
+
+@OPTIONS:
+    -h, --help      This help message.
+    -r, --remove    Remove an item by INDEX number. 
+    -a, --add       Add an item by ITEM.
+    -q, --quiet     No error messages.
+@INDEX:
+    Integers        Index number of item.
+@ITEM:
+    String          Todo ITEM.
+@EXAMPLES:
+    todo            List all items in todo list.
+    todo 1          List 1st ITEM in todo list. 
+    todo -a "Something to do"  Add a todo item.
+    todo -r 1       Remove item at index #1.
+
+EOF
+                return 0
+                ;;
+            1)  # Remove an item
+                if [[ -z "$2" ]]; then
+                    echo "Error: Please provide an index number to remove."
+                    return 1
+                fi
+                if ! [[ "$2" =~ ^[0-9]+$ ]]; then
+                    echo "Error: Index must be a valid positive integer."
+                    return 1
+                fi
+                if ! sed -n "${2}p" "${conf}" >/dev/null; then
+                    echo "Error: No item found at index $2."
+                    return 1
+                fi
+                sed -i -e "${2}d" "${conf}" || {
+                    echo "Error: Failed to remove item."
+                    return 1
+                }
+                echo "Item $2 removed."
+                return 0
+                ;;
+            2)  # Add an item
+                if [[ -z "$2" ]]; then
+                    echo "Error: Please provide an item to add."
+                    return 1
+                fi
+                echo "$2" >> "${conf}" || {
+                    echo "Error: Failed to add item."
+                    return 1
+                }
+                echo "Item added: $2"
+                return 0
+                ;;
+            3)  # List a specific item
+                if ! [[ "$1" =~ ^[0-9]+$ ]]; then
+                    echo "Error: Index must be a valid positive integer."
+                    return 1
+                fi
+                readarray -t array < "${conf}"
+                if [[ -z "${array[$(( $1 - 1 ))]}" ]]; then
+                    echo "Error: No item found at index $1."
+                    return 1
+                fi
+                echo "${array[$(( $1 - 1 ))]}"
+                return 0
+                ;;
+            4)  # Quiet mode
+                return 0
+                ;;
+            5)  # List all items
+                readarray -t array < "${conf}"
+                if [[ ${#array[@]} -eq 0 ]]; then
+                    [[ "${mode}" -ne 4 ]] && echo "No items in the TODO list."
+                    return 0
+                fi
+                for index in "${!array[@]}"; do
+                    echo "[$(( index + 1 ))]: ${array[index]}"
+                done
+                return 0
+                ;;
+        esac
+    else
+        echo "Error: Invalid command. Use -h or --help for usage information."
+        return 1
+    fi
+}
+
+# Tab completion
+if [ "$_" == "$0" ]; then
+    complete -W '-h --help -r --remove -a --add -q --quiet' todo
 else
-	todo "$@"
+    todo "$@"
 fi
